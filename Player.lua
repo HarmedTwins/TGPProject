b502ff0ede11900244fd80992c355fb02052ffee
SMITH Andrew R <s016000f@student.staffs.ac.uk>
2016-11-23 12:38:52 +0000

    Fixed Issues with Slide and Collisions

--- Player.lua
+++ Player.lua
@@ -21,8 +21,6 @@ function init()
 end
 
 function draw()
-  --love.graphics.setColor(255, 0, 0)
-  --love.graphics.rectangle("fill",0, 300, 700, 700)
   
   love.graphics.setColor(255, 255, 255)
   love.graphics.rectangle("fill", x, y, width, height)
@@ -58,7 +56,7 @@ function update(dt)
     slideKeyHeld = true
   end
   
-  if not(slideKey) then
+  if not(slideKey) and CanStand() then
     if slideKeyHeld then
       x = x - (baseWidth - baseHeight)/2
       y = y - baseHeight + baseWidth
@@ -70,6 +68,8 @@ function update(dt)
     slideKeyHeld = false
   end
   
+  onFloor = false
+  
   y = y + velocity * dt
   for i,w in ipairs(Level.Colliders) do
     for j, v in ipairs(Level.Colliders[i]) do
@@ -77,13 +77,11 @@ function update(dt)
        if v.x < x + 32 and x < v.x+v.width  then
           if CheckCollision(x, y, width, height, v.x, v.y, v.width, v.height) then
             if v.lethal then
-              --Restart (TODO)
               main.gamestate = 3
-              --x = v.x - width
             else
               y = v.y - height
               velocity = 0
-              jumpKeyHeld = false
+              onFloor = true
             end
           end
         end
@@ -91,17 +89,8 @@ function update(dt)
     end
   end
   
-  --[[if not(CheckCollision(x, y, width, height, 0, 300, 700, 700)) then
-    y = y + velocity * dt
-  else
-    i = 1
-    while CheckCollision(x, y - i, width, height, 0, 300, 700, 700) do
-      i = i + 1
-    end
-    y = y - i + 1
-    
-    jumpKeyHeld = false
-  end]]--
+  jumpKeyHeld = not(onFloor)
+  
 end
 
 function handleTouch(pos)
@@ -118,4 +107,24 @@ function CheckCollision(x1,y1,w1,h1, x2,y2,w2,h2)
          x2 < x1+w1 and
          y1 < y2+h2 and
          y2 < y1+h1
+end
+
+function CanStand()
+  local canStand = true
+  local tempX = x - (baseWidth - baseHeight)/2
+  local tempY = y - baseHeight + baseWidth
+  
+  for i,w in ipairs(Level.Colliders) do
+    for j, v in ipairs(Level.Colliders[i]) do
+      if not(v == "null") then 
+       if v.x < tempX + 32 and tempX < v.x+v.width  then
+          if CheckCollision(tempX, tempY, baseWidth, baseHeight, v.x, v.y, v.width, v.height) then
+            canStand = false
+          end
+        end
+      end
+    end
+  end
+  
+  return canStand
 end
