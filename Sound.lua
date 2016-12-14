1d5671a2e8b9d2e2c92dd0d53ee6c88ff55de80b
ROBSPC\Robert <b019037f@student.staffs.ac.uk>
2016-12-14 08:07:45 +0000

    +Finished level mapping +Added information page +Added sound/music toggle

--- Sound.lua
+++ Sound.lua
@@ -15,46 +15,68 @@ level4:setLooping(true)
 level5 = love.audio.newSource("assets/level5.wav", "stream")
 level5:setLooping(true)
 jump = love.audio.newSource("assets/jump.wav", "static")
+coin = love.audio.newSource("assets/coin.wav", "static")
 menuselect = love.audio.newSource("assets/menuselect.wav", "static")
+sound = true
 end
 
 function play()
-if main.gamestate == 1 or 2 or 4 then
-love.audio.stop()
-love.audio.play(menu)
-end
+if sound then
+  if main.gamestate == 1 or 2 or 4 then
+    love.audio.stop()
+    love.audio.play(menu)
+  end
 
-if main.gamestate == 3 and main.levelstate == 1 then
-love.audio.stop()
-love.audio.play(level1)
-end
+  if main.gamestate == 3 and main.levelstate == 1 then
+    love.audio.stop()
+    love.audio.play(level1)
+  end
 
-if main.gamestate == 3 and main.levelstate == 2 then
-love.audio.stop()
-love.audio.play(level2)
-end
+  if main.gamestate == 3 and main.levelstate == 2 then
+    love.audio.stop()
+    love.audio.play(level2)
+  end
 
-if main.gamestate == 3 and main.levelstate == 3 then
-love.audio.stop()
-love.audio.play(level3)
-end
+  if main.gamestate == 3 and main.levelstate == 3 then
+    love.audio.stop()
+    love.audio.play(level3)
+  end
 
-if main.gamestate == 3 and main.levelstate == 4 then
-love.audio.stop()
-love.audio.play(level4)
-end
+  if main.gamestate == 3 and main.levelstate == 4 then
+    love.audio.stop()
+    love.audio.play(level4)
+  end
 
-if main.gamestate == 3 and main.levelstate == 5 then
-love.audio.stop()
-love.audio.play(level5)
+  if main.gamestate == 3 and main.levelstate == 5 then
+    love.audio.stop()
+    love.audio.play(level5)
+  end
+else
+  love.audio.stop()  
 end
 end
 
 function playmenuselect()
-love.audio.play(menuselect)
+if sound then  
+  love.audio.play(menuselect)
+else
+  love.audio.stop()
+end
 end
 
 function playjump()
-love.audio.play(jump)
+if sound then
+  love.audio.play(jump)
+else
+  love.audio.stop()
+end
+end
+
+function playcoin()
+if sound then  
+  love.audio.play(coin)
+else
+  love.audio.stop()
+end  
 end
 
